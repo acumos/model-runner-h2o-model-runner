d3bde08f8b9571525209ff1efd998457ebf1768a
rh1832 <rh1832@att.com>
2018-09-12 17:19:00 -0500

    RDS Model Runner
    Adding base swagger code and unit test cases
    Fixed wording for license and other files to reference right predictor
    Added fix in yaml for repo name
    
    Issue-ID: ACUMOS-1225
    
    Change-Id: Ia9307083a67d6c172797bd783919626c1cb3a5fe
    Signed-off-by: rh1832 <rh1832@att.com>

---
+++ setup.py
@@ -0,0 +1,47 @@
+#
+# ===============LICENSE_START=======================================================
+# Acumos
+# ===================================================================================
+# Copyright (C) 2018 AT&T Intellectual Property. All rights reserved.
+# ===================================================================================
+# This Acumos software file is distributed by AT&T
+# under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# This file is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ===============LICENSE_END=========================================================
+from setuptools import setup
+
+
+setup(
+    author='Pavel Kazakov, Ryan Hull',
+    author_email='pk9069@att.com, rh183@att.com',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'License :: OSI Approved :: Apache Software License',
+    ],
+    description=""""RDS Predictor provides the capability to run predictions on RDS / R
+    models and runs predictions either synchronously or asynchronously.""",
+    entry_points="""
+    [console_scripts]
+    rds-model-runner=microservice_flask:main
+    """,
+    keywords='acumos machine learning model runner server predictor rds r ml ai',
+    license='Apache License 2.0',
+    name='predictor-management',
+    python_requires='>=3.4',
+    url='git clone https://gerrit.acumos.org/r/model-runner/rds-model-runner.git',
+    version=1.0,
+)
