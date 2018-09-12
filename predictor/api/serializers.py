268a5f17534864e888656e00e6f367483576788c
rh1832 <rh1832@att.com>
2018-09-12 11:13:24 -0500

    H2O Model Runner
    Adding base swagger code for h2o predictor
    
    Issue-ID; ACUMOS-1225H2O Model Runner
    
    Adding base swagger code for h2o predictor
    
    Issue-ID; ACUMOS-1225
    
    Change-Id: I11bb7b386ab3e402f8b322275ee856d17d9d2d60
    Signed-off-by: rh1832 <rh1832@att.com>

---
+++ predictor/api/serializers.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# ===============LICENSE_START=======================================================
+# Acumos Apache-2.0
+# ===================================================================================
+# Copyright (C) 2017-2018 AT&T Intellectual Property & Tech Mahindra. All rights reserved.
+# ===================================================================================
+# This Acumos software file is distributed by AT&T and Tech Mahindra
+# under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# This file is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ===============LICENSE_END=========================================================
+from flask_restplus import fields
+from predictor.api.namespaces import predictor_namespace as api
+
+async_scoring_fields = api.model("ScoringFields", {
+    "readDatasetKey": fields.String(required=True,
+                                    description='Dataset key to be used for scoring',
+                                    example="jdbc_m09286_string_1529617890081"),
+    "writeDatasetKey": fields.String(required=True, description='Dataset key to be write back results from scoring',
+                                     example="jdbc_m09286_string_1529617890082")
+})
---
+++ setup.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+# ===============LICENSE_START=======================================================
+# Acumos Apache-2.0
+# ===================================================================================
+# Copyright (C) 2017-2018 AT&T Intellectual Property & Tech Mahindra. All rights reserved.
+# ===================================================================================
+# This Acumos software file is distributed by AT&T and Tech Mahindra
+# under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# This file is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ===============LICENSE_END=========================================================
+from setuptools import setup
+
+
+setup(
+    author='Pavel Kazakov, Ryan Hull',
+    author_email='pk9069@att.com, rh183@att.com',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'License :: OSI Approved :: Apache Software License',
+    ],
+    description=""""H2O Predictor provides the capability to run predictions on H2O and POJO
+    and MOJO models and runs predictions either synchronously or asynchronously.""",
+    entry_points="""
+    [console_scripts]
+    h2o-model-runner=microservice_flask:main
+    """,
+    keywords='acumos machine learning model runner server predictor h2o ml ai',
+    license='Apache License 2.0',
+    name='predictor-management',
+    python_requires='>=3.4',
+    url='https://gerrit.acumos.org/r/#/admin/projects/h2o-model-runner.git',
+    version=1.0,
+)
